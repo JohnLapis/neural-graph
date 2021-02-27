723cfb7e5b9d59112b2dd5f5d8f6325fef036e01
john <borges-2014@live.com>
2021-02-27 19:20:44 -0300

    refactor: Move DraggableWrapper to its own file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React from 'react';
+import DraggableWrapper from './DraggableWrapper';
 
 export default function App() {
   return (
@@ -7,59 +8,6 @@ export default function App() {
     </div>
   );
 }
-
-export class DraggableWrapper extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {active: false, currentX: 0, currentY: 0};
-    this.dragStart = this.dragStart.bind(this);
-    this.dragEnd = this.dragEnd.bind(this);
-    this.drag = this.drag.bind(this);
-  }
-
-  dragStart(e) {
-    if (!e.target.parentElement.classList.contains("draggable")) return;
-    this.state.active = true;
-    this.state.initialX = getClientX(e) - this.state.currentX;
-    this.state.initialY = getClientY(e) - this.state.currentY;
-  }
-
-  drag(e) {
-    if (!this.state.active) return;
-    e.preventDefault();
-    this.state.currentX = getClientX(e) - this.state.initialX;
-    this.state.currentY = getClientY(e) - this.state.initialY;
-    const element = e.target.classList.contains("draggable")?
-          e.target.firstChild : e.target;
-    element.style.transform = (
-      `translate3d(${this.state.currentX}px, ${this.state.currentY}px, 0)`
-    );
-  }
-
-  dragEnd() {
-    this.state.active = false;
-  }
-
-  render() {
-    return (
-      <div
-        className="draggable"
-        onMouseDown={this.dragStart}
-        onMouseMove={this.drag}
-        onMouseUp={this.dragEnd}
-        onTouchStart={this.dragStart}
-        onTouchMove={this.drag}
-        onTouchEnd={this.dragEnd}
-      >
-        {this.props.innerElement}
-      </div>
-    )
-  }
-}
-
-const isMobileEvent = e => e.type=="touchmove" || e.type=="touchstart"
-const getClientX = e => (isMobileEvent(e)? e.touches[0]: e).clientX
-const getClientY = e => (isMobileEvent(e)? e.touches[0]: e).clientY
 
 // since ctx is passed around a lot, this should be a class
 function drawDiagonal(ctx, start, end) {
---
+++ src/DraggableWrapper.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+const isMobileEvent = e => e.type === "touchmove" || e.type === "touchstart"
+const getClientX = e => (isMobileEvent(e)? e.touches[0]: e).clientX
+const getClientY = e => (isMobileEvent(e)? e.touches[0]: e).clientY
+
+export default class DraggableWrapper extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {active: false, currentX: 0, currentY: 0};
+    this.dragStart = this.dragStart.bind(this);
+    this.dragEnd = this.dragEnd.bind(this);
+    this.drag = this.drag.bind(this);
+  }
+
+  dragStart(e) {
+    if (!e.target.parentElement.classList.contains("draggable")) return;
+    this.state.active = true;
+    this.state.initialX = getClientX(e) - this.state.currentX;
+    this.state.initialY = getClientY(e) - this.state.currentY;
+  }
+
+  drag(e) {
+    if (!this.state.active) return;
+    e.preventDefault();
+    this.state.currentX = getClientX(e) - this.state.initialX;
+    this.state.currentY = getClientY(e) - this.state.initialY;
+    const element = e.target.classList.contains("draggable")?
+          e.target.firstChild : e.target;
+    element.style.transform = (
+      `translate3d(${this.state.currentX}px, ${this.state.currentY}px, 0)`
+    );
+  }
+
+  dragEnd() {
+    this.state.active = false;
+  }
+
+  render() {
+    return (
+      <div
+        className="draggable"
+        onMouseDown={this.dragStart}
+        onMouseMove={this.drag}
+        onMouseUp={this.dragEnd}
+        onTouchStart={this.dragStart}
+        onTouchMove={this.drag}
+        onTouchEnd={this.dragEnd}
+      >
+        {this.props.innerElement}
+      </div>
+    )
+  }
+}
